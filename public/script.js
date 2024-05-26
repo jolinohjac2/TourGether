603a1c9ec79943a1063bc2914465cb0f6b7294a1
jolinohjac2 <jolinohjac2@gmail.com>
2024-05-26 16:32:34 +0000

    voting table

--- public/script.js
+++ public/script.js
@@ -12,13 +12,14 @@ document.getElementById('questionsForm').addEventListener('submit', function(eve
     // Get form data
     const destination = document.getElementById('question1').value;
     const duration = document.getElementById('question2').value;
+    const people = document.getElementById('question3').value;
 
     // Define the conditions for redirection
     let redirectUrl = '';
 
     // Check the values of the form inputs and set the redirect URL accordingly
-    if (destination === 'Singapore' && duration === '1') {
-        redirectUrl = 'result_singapore_1.html';
+    if (destination === 'Singapore' && duration === '1' && people === '3') {
+        redirectUrl = 'result_singapore_1_3pax.html';
     } else if (destination === 'Singapore' && duration === '2') {
         redirectUrl = 'result_singapore_2.html';
     } else if (destination === 'Bangkok' && duration === '1') {
